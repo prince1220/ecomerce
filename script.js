a98b2285ac864cfb27a8d50a1e369db8593ad329
prince1220 <frontdeskdesigns@gamil.com>
2024-03-10 20:42:29 -0400

    Created third product

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL= "https://api.npoint.io/eb5a4cb6302afcdb43b9"
+const API_URL= "https://api.npoint.io/67616547e8742db57378"
 ///priceEl.innerHTML="2"
 function loadData(){ 
   fetch(API_URL).then(res=>res.json()).then(data=>{
@@ -22,27 +22,19 @@ function loadData(){
     priceEl.innerHTML = data.product1.price
     nameEl.innerHTML = data.product1.name
     imageEl.src=data.product1.img
-
-    console.log(data)
-    console.log(data.product2)
-    console.log(data.product2.price)
-    console.log(data.product2.name)
     
-    /*
-     {
-       "product1":{
-    "name":"BMW M3",
-    "price":"$100",
-    "img":"https://s1.cdn.autoevolution.com/images/news/here-s-a-bmw-e30-m3-that-almost-anyone-can-afford-photo-gallery-64396_1.jpg"
-        }
-     }
 
-
-    */
+ 
     
     priceEl2.innerHTML = data.product2.price
     nameEl2.innerHTML = data.product2.name
     imageEl2.src=data.product2.img
+
+
+
+    priceEl3.innerHTML = data.product3.price
+    nameEl3.innerHTML = data.product3.name
+    imageEl3.src=data.product3.img
 
     
 
